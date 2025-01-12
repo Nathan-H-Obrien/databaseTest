9e6017ff9438eee25dcd0dcaf823ab6f7f613eed
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2025-01-12 12:51:20 -0700

    Add files via upload

--- Login.py
+++ Login.py
@@ -8,31 +8,31 @@ def Login_page():
     email = st.text_input('Email:')
     password = st.text_input('Password:', type='password')
     if st.button('Login', key='login'):
-            with sqlite3.connect('test.db') as conn:
-                cursor = conn.execute('SELECT * FROM admin WHERE email = ? AND password = ?', (email, password))
-                cursor2 = conn.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, password))
-                if cursor.fetchone():
-                    st.write('Welcome admin')
-                    if st.button("Add User", key='add_user_admin'):
-                            st.write('Add a user')
+        with sqlite3.connect('test.db') as conn:
+            cursor = conn.execute('SELECT * FROM admin WHERE email = ? AND password = ?', (email, password))
+            cursor2 = conn.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, password))
+            if cursor.fetchone():
+                st.write('Welcome admin')
+                if st.button("Add User", key='add_user_admin'):
+                    st.write('Add a user')
+                    with sqlite3.connect('test.db') as conn:
+                        id = random.randint(1, 999999)
+                        while conn.execute('SELECT * FROM users WHERE id = ?', (id,)).fetchone():
+                            id = random.randint(1, 999999)
+                        username = st.text_input('Name: ')
+                        email = st.text_input('Email: ')
+                        password = st.text_input('Password: ', type='password')
+                        created_at = datetime.now()
+                        created_at = created_at.strftime('%Y-%m-%d %H:%M:%S')
+                        if st.button('Create account', key='create_account'):
                             with sqlite3.connect('test.db') as conn:
-                                id = random.randint(1, 999999)
-                                while conn.execute('SELECT * FROM users WHERE id = ?', (id,)).fetchone():
-                                    id = random.randint(1, 999999)
-                                username = st.text_input('Name: ')
-                                email = st.text_input('Email: ')
-                                password = st.text_input('Password: ', type='password')
-                                created_at = datetime.now()
-                                created_at = created_at.strftime('%Y-%m-%d %H:%M:%S')
-                            if st.button('Create account', key='create_account'):
-                                    with sqlite3.connect('test.db') as conn:
-                                        conn.execute('INSERT INTO users (id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)', 
-                                        (id, username, email, password, created_at))
-                                        conn.commit()
+                                conn.execute('INSERT INTO users (id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)', 
+                                (id, username, email, password, created_at))
+                                conn.commit()
                     if st.button("View Users", key='view_users_admin'):
-                                with sqlite3.connect('test.db') as conn:
-                                    for row in conn.execute('SELECT * FROM users'):
-                                        st.write(row)
+                        with sqlite3.connect('test.db') as conn:
+                            for row in conn.execute('SELECT * FROM users'):
+                                st.write(row)
                         
                     if st.button("Logout", key='logout_admin'):
                         exit()
