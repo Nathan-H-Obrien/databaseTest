1bc0ad3464a6819c87994329da1b476f894b0e28
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2025-01-12 11:48:06 -0700

    Add files via upload

--- CreateAccount.py
+++ CreateAccount.py
@@ -18,7 +18,7 @@ def Create_account_page():
             password = st.text_input('Password: ', type='password')
             created_at = datetime.now()
             created_at = created_at.strftime('%Y-%m-%d %H:%M:%S')
-            if st.button('Create account'):
+            if st.button('Create account', key='create_account'):
                 with sqlite3.connect('test.db') as conn:
                     conn.execute('INSERT INTO users (id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)', 
                     (id, username, email, password, created_at))
