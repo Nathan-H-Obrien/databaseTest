f681fa8363acd1473729b12ef11af5df3ccf2ea3
Nathan-H-Obrien <143020470+Nathan-H-Obrien@users.noreply.github.com>
2025-01-12 11:27:51 -0700

    Add files via upload

--- Login.py
+++ Login.py
@@ -7,13 +7,15 @@ def Login_page():
     st.title('Login')
     email = st.text_input('Email:')
     password = st.text_input('Password:', type='password')
-    if st.button('Login', key='login'):
+    st.button('Login', key='login', on_click=login(email, password))
+def login(email, password):
             with sqlite3.connect('test.db') as conn:
                 cursor = conn.execute('SELECT * FROM admin WHERE email = ? AND password = ?', (email, password))
                 if cursor.fetchone():
                     st.write('Welcome admin')
                     while True:
-                        if st.button("Add User", key='add_user'):
+                        st.button("Add User", key='add_user', on_click=add_user())
+                        def add_user():    
                             st.write('Add a user')
                             with sqlite3.connect('test.db') as conn:
                                 id = random.randint(1, 999999)
@@ -32,27 +34,28 @@ def Login_page():
                                         conn.execute('INSERT INTO users (id, username, email, password, created_at) VALUES (?, ?, ?, ?, ?)', 
                                         (id, username, email, password, created_at))
                                         conn.commit()
-                        if st.button("View Users"):
+                        st.button("View Users", key='view_users', on_click=view_users())
+                        def view_users():
                             try:
                                 with sqlite3.connect('test.db') as conn:
                                     for row in conn.execute('SELECT * FROM users'):
                                         print(row)
                             except sqlite3.Error as e:
                                 st.write('No users found')
-                        if st.button("Exit"):
-                            exit()
+                        
+                        st.button("Exit", key='exit', on_click=exit())
                 elif cursor.fetchone() is None:
                     cursor = conn.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, password))
                     cursor.fetchone()
                     if cursor.fetchone():
                         st.write('Welcome user')
                         while True:
-                            if st.button('View profile'):
+                            st.button('View profile', key='view_profile', on_click=view_profile())
+                            def view_profile():
                                 with sqlite3.connect('test.db') as conn:
                                     cursor = conn.execute('SELECT * FROM users WHERE email = ?', (email,))
                                     for row in cursor:
                                         print(row)
-                            if st.button('Exit'):
-                                exit()
+                            st.button('Exit', key='exit', on_click=exit())
                 else:
                     st.write('Invalid username or password')
